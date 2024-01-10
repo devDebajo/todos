8696d01fa20309e4a4ebd804c404bc8d9c307a2a
debajo <nikitosbobin@gmail.com>
2024-01-10 23:28:52 +0500

    only in bytes encryption

--- composeApp/src/commonMain/kotlin/ru/debajo/todos/data/db/EncryptedSqlDriver.kt
+++ composeApp/src/commonMain/kotlin/ru/debajo/todos/data/db/EncryptedSqlDriver.kt
@@ -4,14 +4,17 @@ import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlCursor
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlPreparedStatement
+import java.nio.charset.Charset
 import ru.debajo.todos.security.AesHelper
-import ru.debajo.todos.security.decryptString
-import ru.debajo.todos.security.encryptString
 
 class EncryptedSqlDriver(
     private val delegate: SqlDriver,
     private val secret: String,
 ) : SqlDriver by delegate {
+
+    private val bytesDecryptor: (ByteArray) -> ByteArray = { AesHelper.decryptBytes(secret, it) }
+    private val bytesEncryptor: (ByteArray) -> ByteArray = { AesHelper.encryptBytes(secret, it) }
+
     override fun execute(
         identifier: Int?,
         sql: String,
@@ -25,7 +28,7 @@ class EncryptedSqlDriver(
             binders = if (binders == null) {
                 null
             } else {
-                { EncryptedSqlPreparedStatement(this, secret).binders() }
+                { EncryptedSqlPreparedStatement(this, bytesEncryptor).binders() }
             }
         )
     }
@@ -40,12 +43,12 @@ class EncryptedSqlDriver(
         return delegate.executeQuery(
             identifier = identifier,
             sql = sql,
-            mapper = { cursor -> mapper(EncryptedSqlCursor(cursor, secret)) },
+            mapper = { cursor -> mapper(EncryptedSqlCursor(cursor, bytesDecryptor)) },
             parameters = parameters,
             binders = if (binders == null) {
                 null
             } else {
-                { EncryptedSqlPreparedStatement(this, secret).binders() }
+                { EncryptedSqlPreparedStatement(this, bytesEncryptor).binders() }
             }
         )
     }
@@ -53,44 +56,37 @@ class EncryptedSqlDriver(
 
 private class EncryptedSqlCursor(
     private val delegate: SqlCursor,
-    private val secret: String,
+    private val bytesDecryptor: (ByteArray) -> ByteArray,
 ) : SqlCursor by delegate {
+
+    override fun getBytes(index: Int): ByteArray? {
+        val encryptedBytes = delegate.getBytes(index) ?: return null
+        return bytesDecryptor(encryptedBytes)
+    }
+
     override fun getString(index: Int): String? {
-        val encryptedString = delegate.getString(index) ?: return null
-        return AesHelper.decryptString(secret, encryptedString)
+        val stringBytes = getBytes(index) ?: return null
+        return String(stringBytes, Charset.defaultCharset())
     }
 
     override fun getBoolean(index: Int): Boolean? = getString(index)?.toBooleanStrict()
 
-    override fun getBytes(index: Int): ByteArray? {
-        val encryptedBytes = delegate.getBytes(index) ?: return null
-        return AesHelper.decryptBytes(secret, encryptedBytes)
-    }
-
     override fun getLong(index: Int): Long? = getString(index)?.toLong()
 
-    override fun getDouble(index: Int): Double? {
-        val bits = getLong(index) ?: return null
-        return Double.fromBits(bits)
-    }
+    override fun getDouble(index: Int): Double? = getLong(index)?.let { Double.fromBits(it) }
 }
 
 private class EncryptedSqlPreparedStatement(
     private val delegate: SqlPreparedStatement,
-    private val secret: String,
+    private val bytesEncryptor: (ByteArray) -> ByteArray,
 ) : SqlPreparedStatement by delegate {
-    override fun bindString(index: Int, string: String?) {
-        delegate.bindString(
-            index = index,
-            string = string?.let { AesHelper.encryptString(secret, it) },
-        )
-    }
 
     override fun bindBytes(index: Int, bytes: ByteArray?) {
-        delegate.bindBytes(
-            index = index,
-            bytes = bytes?.let { AesHelper.encryptBytes(secret, it) },
-        )
+        delegate.bindBytes(index, bytes?.let(bytesEncryptor))
+    }
+
+    override fun bindString(index: Int, string: String?) {
+        bindBytes(index, string?.toByteArray(Charset.defaultCharset()))
     }
 
     override fun bindBoolean(index: Int, boolean: Boolean?) {
